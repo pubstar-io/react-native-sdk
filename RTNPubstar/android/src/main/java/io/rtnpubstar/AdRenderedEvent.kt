02b66228d837d3cbcf29a32f186f68fdef809e84
Elon Nguyen <elon.nguyen@tqcsolution.com>
2025-08-05 15:19:34 +0700

    update adRenderEvent

--- RTNPubstar/android/src/main/java/io/rtnpubstar/AdRenderedEvent.kt
+++ RTNPubstar/android/src/main/java/io/rtnpubstar/AdRenderedEvent.kt
@@ -3,29 +3,11 @@ package io.rtnpubstar
 import com.facebook.react.uimanager.events.Event
 import com.facebook.react.uimanager.events.RCTModernEventEmitter
 
-class AdLoadedEvent(
-    surfaceId: Int,
-    viewTag: Int
-) : Event<AdLoadedEvent>(surfaceId, viewTag) {
-    override fun getEventName(): String = "onLoaded"
-
-    override fun canCoalesce(): Boolean = false
-
-    override fun dispatchModern(rctEventEmitter: RCTModernEventEmitter) {
-        rctEventEmitter.receiveEvent(
-            surfaceId,
-            viewTag,
-            eventName,
-            null
-        )
-    }
-}
-
 class AdEvent(
     private val name: String,
     surfaceId: Int,
     viewTag: Int,
-) : Event<AdLoadedEvent>(surfaceId, viewTag) {
+) : Event<AdEvent>(surfaceId, viewTag) {
     override fun getEventName(): String = this.name
 
     override fun canCoalesce(): Boolean = false
