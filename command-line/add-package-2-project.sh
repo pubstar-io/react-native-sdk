47b39c9bfeda0dde4a1969543454189565bebe36
Elon Nguyen <elon.nguyen@tqcsolution.com>
2025-08-15 14:35:07 +0700

    add npmignore

--- command-line/add-package-2-project.sh
+++ command-line/add-package-2-project.sh
@@ -6,8 +6,8 @@ echo "=== Add the NPM package to ExampleApp ==="
 cd ../ExampleApp
 yarn add ../RTNPubstar
 
-echo "=== Install the new dependencies in iOS Example ==="
-cd ios
-RCT_NEW_ARCH_ENABLED=1 bundle exec pod install
+# echo "=== Install the new dependencies in iOS Example ==="
+# cd ios
+# RCT_NEW_ARCH_ENABLED=1 bundle exec pod install
 
 echo "=== DONE: package 'react-native-pubstar' added to ExampleApp ==="
