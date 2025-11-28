995bc233a9a4dbb18054921680755dbe711d635a
Elon Nguyen <elon.nguyen@tqcsolution.com>
2025-11-28 17:00:51 +0700

    Release rtn_pubstar 1.3.1

--- RTNPubstar/android/src/main/java/io/rtnpubstar/PubstarModule.kt
+++ RTNPubstar/android/src/main/java/io/rtnpubstar/PubstarModule.kt
@@ -1,43 +1,61 @@
 package io.rtnpubstar
 
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import com.facebook.react.bridge.Callback
 import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import io.pubstar.NativeRTNPubstarSpec
-import io.pubstar.mobile.ads.interfaces.AdLoaderListener
-import io.pubstar.mobile.ads.interfaces.AdShowedListener
-import io.pubstar.mobile.ads.interfaces.InitAdListener
-import io.pubstar.mobile.ads.interfaces.PubStarAdController
-import io.pubstar.mobile.ads.model.ErrorCode
-import io.pubstar.mobile.ads.model.RewardModel
-import io.pubstar.mobile.ads.pub.PubStarAdManager
+import io.pubstar.mobile.core.api.PubStarAdManager
+import io.pubstar.mobile.core.interfaces.AdLoaderListener
+import io.pubstar.mobile.core.interfaces.AdShowedListener
+import io.pubstar.mobile.core.interfaces.InitAdListener
+import io.pubstar.mobile.core.interfaces.PubStarAdController
+import io.pubstar.mobile.core.models.ErrorCode
+import io.pubstar.mobile.core.models.RewardModel
 
 class PubstarModule(reactContext: ReactApplicationContext) : NativeRTNPubstarSpec(reactContext) {
     private val pubStarAdController: PubStarAdController by lazy {
         PubStarAdManager.getAdController()
     }
 
-  override fun getName() = NAME
+    override fun getName() = NAME
 
-  override fun initialization(promise: Promise) {
-    PubStarAdManager.getInstance()
-      .setInitAdListener(object : InitAdListener {
-          override fun onDone() {
-              promise.resolve(null)
-          }
+    private fun runOnMainThread(block: () -> Unit) {
+        if (Looper.myLooper() == Looper.getMainLooper()) {
+            block()
+        } else {
+            Handler(Looper.getMainLooper()).post { block() }
+        }
+    }
 
-          override fun onError(code: ErrorCode) {
-              promise.reject("INIT_FAILED", code.name)
-          }
-      })
-      .init(reactApplicationContext)
-  }
+    override fun initialization(promise: Promise) {
+        runOnMainThread {
+            PubStarAdManager.getInstance()
+                .setInitAdListener(object : InitAdListener {
+                    override fun onDone() {
+                        Log.d("[SDK-Native]", "initialization onDane called")
+                        promise.resolve(null)
+                    }
+
+                    override fun onError(code: ErrorCode) {
+                        Log.d(
+                            "[SDK-Native]",
+                            "initialization error: ${code.code} ${code.name} called"
+                        )
+                        promise.reject("INIT_FAILED", code.name)
+                    }
+                })
+                .init(reactApplicationContext)
+        }
+    }
 
     override fun loadAd(adId: String?, onError: Callback?, onLoaded: Callback?) {
         pubStarAdController.load(
             reactApplicationContext,
             adId,
-            object: AdLoaderListener {
+            object : AdLoaderListener {
                 override fun onError(code: ErrorCode) {
                     if (onError != null) {
                         onError(code)
@@ -59,7 +77,7 @@ class PubstarModule(reactContext: ReactApplicationContext) : NativeRTNPubstarSpe
             reactApplicationContext,
             adId,
             null,
-            object: AdShowedListener {
+            object : AdShowedListener {
                 override fun onAdHide(any: RewardModel?) {
                     if (onHide != null) {
                         onHide(any)
@@ -82,44 +100,44 @@ class PubstarModule(reactContext: ReactApplicationContext) : NativeRTNPubstarSpe
     }
 
     override fun loadAndShow(
-      adId: String,
-      onLoadError: Callback,
-      onLoaded: Callback,
-      onAdHide: Callback,
-      onAdShowed: Callback,
-      onShowError: Callback
-  ) {
-      pubStarAdController.loadAndShow(
-          reactApplicationContext,
-          adId,
-          null,
-          object : AdLoaderListener {
-              override fun onError(code: ErrorCode) {
-                  onLoadError.invoke(code.name)
-              }
+        adId: String,
+        onLoadError: Callback,
+        onLoaded: Callback,
+        onAdHide: Callback,
+        onAdShowed: Callback,
+        onShowError: Callback
+    ) {
+        pubStarAdController.loadAndShow(
+            reactApplicationContext,
+            adId,
+            null,
+            object : AdLoaderListener {
+                override fun onError(code: ErrorCode) {
+                    onLoadError.invoke(code.name)
+                }
 
-              override fun onLoaded() {
-                  onLoaded.invoke("onLoaded")
-              }
-          },
-          object : AdShowedListener {
-              override fun onAdHide(any: RewardModel?) {
-                  val rewardMap = rewardToMap(any)
-                  onAdHide.invoke(rewardMap)
-              }
+                override fun onLoaded() {
+                    onLoaded.invoke("onLoaded")
+                }
+            },
+            object : AdShowedListener {
+                override fun onAdHide(any: RewardModel?) {
+                    val rewardMap = rewardToMap(any)
+                    onAdHide.invoke(rewardMap)
+                }
 
-              override fun onAdShowed() {
-                  onAdShowed.invoke()
-              }
+                override fun onAdShowed() {
+                    onAdShowed.invoke()
+                }
 
-              override fun onError(code: ErrorCode) {
-                  onShowError.invoke(code.name)
-              }
-          }
-      )
-  }
+                override fun onError(code: ErrorCode) {
+                    onShowError.invoke(code.name)
+                }
+            }
+        )
+    }
 
-  companion object {
-    const val NAME = "RTNPubstar"
-  }
+    companion object {
+        const val NAME = "RTNPubstar"
+    }
 }
--- RTNPubstar/android/src/main/java/io/rtnpubstar/PubstarPackage.kt
+++ RTNPubstar/android/src/main/java/io/rtnpubstar/PubstarPackage.kt
@@ -8,29 +8,29 @@ import com.facebook.react.module.model.ReactModuleInfoProvider
 import com.facebook.react.uimanager.ViewManager
 
 class PubstarPackage : BaseReactPackage() {
-  override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
-  if (name == PubstarModule.NAME) {
-    PubstarModule(reactContext)
-  } else {
-    null
-  }
+    override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? =
+        if (name == PubstarModule.NAME) {
+            PubstarModule(reactContext)
+        } else {
+            null
+        }
 
- override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
-   mapOf(
-     PubstarModule.NAME to ReactModuleInfo(
-       PubstarModule.NAME,
-       PubstarModule.NAME,
-       false, // canOverrideExistingModule
-       false, // needsEagerInit
-       false, // isCxxModule
-       true // isTurboModule
-     )
-   )
- }
+    override fun getReactModuleInfoProvider() = ReactModuleInfoProvider {
+        mapOf(
+            PubstarModule.NAME to ReactModuleInfo(
+                PubstarModule.NAME,
+                PubstarModule.NAME,
+                false, // canOverrideExistingModule
+                false, // needsEagerInit
+                false, // isCxxModule
+                true // isTurboModule
+            )
+        )
+    }
 
-  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
-    listOf(PubstarAdViewManager())
+    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> =
+        listOf(PubstarAdViewManager())
 
-  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
-    emptyList()
+    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> =
+        emptyList()
 }
